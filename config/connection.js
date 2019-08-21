f0737ca9dbfd3398f66f9dcbed3773c883b77161
ab1228 <alexb8734@gmail.com>
2019-08-21 11:33:09 -0500

    config for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,39 @@
 require("dotenv").config()
-const Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+require("dotenv").config();
+// require mysql
+var mysql = require('mysql');
+
+
+var config = {
+    host: DB_HOST,
     port: 3306,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
+    user: DB_USER,
+    // grab password from .env
+    password: DB_PASS,
+    database: DB_NAME
+}
+var connection;
+
+var host;
+
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    host = 'JAWSDB';
+} else {
+    connection = mysql.createConnection(config);
+    host = 'localhost';
+}
+
+// connect to database
+connection.connect((err) => {
+    if (err) {
+        console.log("error connecting: " + err.stack);
+        return;
     }
-
-
-
-
+    console.log("connected");
 });
-sequelize.authenticate()
-    .then(() => console.log('Database connected'))
-    .catch(err => console.log('Error' + err))
+
+// export connection
+module.exports = conn;
+
